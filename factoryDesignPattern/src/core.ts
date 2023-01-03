ca7092061e725d8905282d7f6c04de66f665c030
OGabrielPereira <ogabrielpereira@pm.me>
2023-01-03 15:57:59 -0300

    Factory commit

---
+++ factoryDesignPattern/src/core.ts
@@ -0,0 +1,20 @@
+
+function createCore() {
+
+
+    function start() {
+        console.log("> [core] Starting...");
+
+        console.log("> [core] Starting done! System's running!");
+    }
+
+    function stop() {
+        console.log("> [core] Stopping...");
+
+        console.log("> [core] Stopping done!");
+    }
+
+    return {start, stop}
+}
+
+export default createCore;
